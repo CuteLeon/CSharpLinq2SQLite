6009e506aba63275c0f748633af99b6b18240175
Leon <xiaoyankeji@hotmail.com>
2018-06-13 15:50:55 +0800

    完成 DotNetCore 平台上 SQLite 数据库基于EntityFrameworkCore框架下的程序Demo；

---
+++ DotNetCoreLinq2SQLite/EFCoreMigration.bat
@@ -0,0 +1,3 @@
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+echo 数据库迁移完成。
---
+++ DotNetCoreLinq2SQLite/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetCoreLinq2SQLite.Models;
+
+namespace DotNetCoreLinq2SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreSQLiteDBContext DBContext = new CoreSQLiteDBContext();
+            DBContext.SaveChanges();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
